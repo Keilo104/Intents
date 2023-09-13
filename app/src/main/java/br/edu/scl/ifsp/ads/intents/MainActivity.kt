54f14226a19ac88f9d0fba0d6f01a7d67cc3d810
keilo <keilogamimg@gmail.com>
2023-09-12 22:27:49 -0300

    implementing the "intent" part of the intent app

--- app/src/main/java/br/edu/scl/ifsp/ads/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/intents/MainActivity.kt
@@ -1,10 +1,20 @@
 package br.edu.scl.ifsp.ads.intents
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_PICK
+import android.content.Intent.ACTION_VIEW
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
+import android.os.Environment
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.ads.intents.databinding.ActivityMainBinding
@@ -15,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var parl: ActivityResultLauncher<Intent>
+    private lateinit var permissaoChamadaArl: ActivityResultLauncher<String>
+    private lateinit var pegarImagemArl: ActivityResultLauncher<Intent>
 
     companion object {
         const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
@@ -35,6 +47,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        permissaoChamadaArl =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            permissaoConcedida ->
+            if(permissaoConcedida) {
+                chamarNumero(true)
+
+            } else {
+                Toast.makeText(this, "Sem permissão, sem chamada!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+        pegarImagemArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+            if (result.resultCode == RESULT_OK) {
+                // uma imagem foi selecionada
+                val imagemUri = result.data?.data
+                imagemUri?.let {
+                    amb.parametroTv.text = imagemUri.toString()
+                    val visualizarIntent = Intent(ACTION_VIEW, imagemUri)
+                    startActivity(visualizarIntent)
+                }
+            }
+        }
+
         amb.entrarParametroBt.setOnClickListener {
             val parametroIntent: Intent = Intent("PARAMETRO_ACTIVITY_ACTION")
             parametroIntent.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
@@ -42,4 +79,68 @@ class MainActivity : AppCompatActivity() {
             parl.launch(parametroIntent)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.viewMi -> {
+                val url = Uri.parse(amb.parametroTv.text.toString())
+                val navegadorIntent = Intent(ACTION_VIEW, url)
+                startActivity(navegadorIntent)
+                true
+            }
+
+            R.id.callMi -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    // Verificar permissão
+                    if (checkSelfPermission(CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                        chamarNumero(true)
+                    } else {
+                        // Solicitar a permissão
+                        permissaoChamadaArl.launch(CALL_PHONE)
+                    }
+
+                } else {
+                    // Permissão já foi dada durante a instalação pq o Android é menor que M
+                    chamarNumero(true)
+                }
+
+                true
+            }
+
+            R.id.dialMi -> {
+                chamarNumero(false)
+                true
+            }
+
+            R.id.pickMi -> {
+                val pegarImagemIntent = Intent(ACTION_PICK)
+                val diretorioImagens = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    .path
+
+                pegarImagemIntent.setDataAndType(Uri.parse(diretorioImagens), "image/*")
+                pegarImagemArl.launch(pegarImagemIntent)
+                true
+            }
+
+            R.id.chooserMi -> {
+                true
+            }
+
+            else -> false
+        }
+    }
+
+    private fun chamarNumero(chamar: Boolean) {
+        val numeroUri = Uri.parse("tel: ${amb.parametroTv.text}")
+        val intentMuitoLoca = Intent(if(chamar) ACTION_CALL else ACTION_DIAL)
+        intentMuitoLoca.data = numeroUri
+        startActivity(intentMuitoLoca)
+    }
 }
+
